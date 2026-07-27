6cb01d57fd5fb2ad88a5519670d2f1f689bdb185
swaglord03 <lrparrish03@gmail.com>
2026-07-27 12:31:30 -0700

    Update trainer-config.ts

--- src/data/trainers/trainer-config.ts
+++ src/data/trainers/trainer-config.ts
@@ -5639,6 +5639,7 @@ export const trainerConfigs: TrainerConfigs = {
     .setModifierRewardFuncs(
   () => modifierTypes.TERA_ORB,
   () => modifierTypes.MEGA_BRACELET,
+      () => modifierTypes.MAX_MUSHROOM,
 )
     .setPartyMemberFunc(0, getRandomRivalPartyMemberFunc(RIVAL_4_POOL, 0))
     .setPartyMemberFunc(1, getRandomRivalPartyMemberFunc(RIVAL_4_POOL, 1))
